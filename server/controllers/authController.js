1bbd67a08eab11dee33255f22ef8617199c8db4d
Jimpulse48 <107091480+Jimpulse48@users.noreply.github.com>
2023-05-13 13:15:28 -0500

    completed initial validateuser functionality

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,6 +2,22 @@ const Users = '../models/usersModel';
 
 const authController = {};
 
-authController.validateuser = (req, res, next) => {};
+authController.validateuser = (req, res, next) => {
+  console.log('-----> Attempting to Validate User!');
+  const { username, password } = req.query;
+
+  Users.find({ username, password })
+    .then((response) => {
+      console.log('-----> User Validated');
+      res.locals.storage = response;
+      return next();
+    })
+    .catch((error) => {
+      return next({
+        log: 'Express error handler caught! authController.validateuser middleware error',
+        message: error,
+      });
+    });
+};
 
 module.exports = authController;
