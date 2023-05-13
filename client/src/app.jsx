450b33160ebef7f837b0ab44aed18ebddb929660
Jonathan Tsai <jonathantsai@jonathans-mbp.mynetworksettings.com>
2023-05-13 14:36:57 -0400

    added boiler plate code to login and register

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,11 +1,14 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Calendar from "./components/Calendar";
-import Input from "./components/"
-import Login from "./components/Login";
-import Register from "./components/Register"
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Calendar from './components/Calendar';
+import Register from './components/Register';
+import Auth from './components/Auth';
+import { useSelector, useDispatch } from 'react-redux';
 
 const App = () => {
+  <Routes>
+    <Route path="/" element={<Auth />} />
+  </Routes>;
+};
 
-    
-}
+export default App;
