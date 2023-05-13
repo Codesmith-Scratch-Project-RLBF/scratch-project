f2eaa36784250d2b0f1af9b0ece4f009dc64a6a1
Jimpulse48 <107091480+Jimpulse48@users.noreply.github.com>
2023-05-13 14:16:48 -0500

    switched to findOne
    added conditional logic for no users found

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -6,15 +6,20 @@ authController.validateuser = (req, res, next) => {
   console.log('-----> Attempting to Validate User!');
   const { username, password } = req.query;
 
-  Users.find({ username, password })
+  Users.findOne({ username, password })
     .then((response) => {
-      console.log('-----> User Validated');
-      res.locals.storage = response;
+      
+      if (response) {
+        console.log('-----> User Validated');
+        res.locals.storage = response;
+      }
+
       return next();
     })
     .catch((error) => {
       return next({
         log: 'Express error handler caught! authController.validateuser middleware error',
+        status: 400,
         message: error,
       });
     });
