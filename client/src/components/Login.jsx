114be30559ccce084a4f4fadd698fec9dd8dbf51
Jonathan Tsai <jonathantsai@jonathans-mbp.mynetworksettings.com>
2023-05-13 14:54:25 -0400

    made changes with jimmy's comments

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -6,16 +6,24 @@ const Login = () => {
 
   const handleLogin = async () => {};
 
+  const handleUsername = (e) => {
+    setUserName(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassWord(e.target.value);
+  };
+
   return (
     <div className="login-button">
       <form onSubmit={handleLogin}>
         <label>
           <h1>Username:</h1>
-          <input type="text" onChange={setUserName()} />
+          <input type="text" handleUsername={setUserName()} />
         </label>
         <label>
           <h1>Password:</h1>
-          <input type="text" onChange={setPassWord()} />
+          <input type="text" handlePassword={setPassWord()} />
         </label>
         <button>Log-in</button>
       </form>
