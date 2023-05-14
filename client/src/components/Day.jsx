d4cf2e14e5bc8f55540dad6ecb22c128e9926620
Jonathan Tsai <jonathantsai@jonathans-mbp.mynetworksettings.com>
2023-05-13 22:20:27 -0400

    finished with calendar, day, and time components

--- client/src/components/Day.jsx
+++ client/src/components/Day.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-
+import Time from './Time'
 const Day = (props) => {
-  const time =  props.time;
+  const times = props.times;
   const timeSlotArray = [
     '12:00 am',
     '12:30 am',
@@ -29,28 +29,36 @@ const Day = (props) => {
     '11:00 am',
     '11:30 am',
     '12:00 pm',
-    '12:00 pm',
-    '12:00 pm',
-    '12:00 pm',
-    '12:00 pm',
-    '12:00 pm',
-    '12:00 pm',
-    '12:00 pm',
-    '12:00 pm',
-    '12:00 pm',
-    '12:00 pm',
-    '12:00 pm',
-    '12:00 pm',
-    '12:00 pm',
+    '12:30 pm',
+    '1:00 pm',
+    '1:30 pm',
+    '2:00 pm',
+    '2:30 pm',
+    '3:00 pm',
+    '3:30 pm',
+    '4:00 pm',
+    '4:30 pm',
+    '5:00 pm',
+    '5:30 pm',
+    '6:00 pm',
+    '7:00 pm',
+    '7:30 pm',
+    '8:00 pm',
+    '8:30 pm',
+    '9:00 pm',
+    '9:30 pm',
+    '10:00 pm',
+    '10:30 pm',
+    '11:00 pm',
+    '11:30 pm',
   ];
   return (
     <div>
-      {/* {timeFrames.forEach(element){
-        if (element){
-            return <Time/>
-        }
-        
-    }} */}
+      {times.map((element, index) => {
+        return (
+          <Time key={index} time={timeSlotArray[index]} isColored={times} />
+        );
+      })}
     </div>
   );
 };
