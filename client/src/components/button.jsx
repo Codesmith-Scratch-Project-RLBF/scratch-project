589209c6a517df8bc7b0c6bbf01ea842fe58e368
Shahmar Aliyev <shahmaraliyev@Shahmars-MacBook-Pro.local>
2023-05-14 15:12:43 -0400

    added http requests

--- client/src/components/button.jsx
+++ client/src/components/button.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import './button.scss';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteUser, logoutUser } from '../redux/userSlice';
 const Button = () => {
-  const handleDelete = async () => {};
-  const handleLogout = async () => {};
+  const userName = useSelector((state) => {
+    return state.user.userName;
+  });
+  const dispatch = useDispatch();
+  const handleDelete = async () => {
+    try {
+      const response = await axios.delete(`/users/${userName}/`);
+      const deletedUser = await response.json();
+      dispatch(deleteUser());
+      navigate('/');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleLogout = async () => {
+    dispatch(logoutUser());
+  };
   return (
     <div className='buttons'>
       <button oncClick={handleLogout}>Log out</button>
